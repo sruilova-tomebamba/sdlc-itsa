2360030ac91b9ca00669c00db28c53c2251b7903
maria.reyes <maria.reyes@tomebamba.com.ec>
2025-01-28 08:13:47 -0500

    feat: Agregando funcion suma

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+# Pedir al usuario que ingrese dos números
+numero1 = int(input("Introduce el primer número: "))
+numero2 = int(input("Introduce el segundo número: "))
+
+# Sumar los números
+resultado = numero1 + numero2
+
+# Imprimir el resultado
+print("La suma es:", resultado)
