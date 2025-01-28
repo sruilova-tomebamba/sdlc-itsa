c6c018ff05cfa3801500f1caaede1bee4fe9046c
maria.reyes <maria.reyes@tomebamba.com.ec>
2025-01-28 09:44:40 -0500

    feat: Agregando formatos fecha

--- format_utilities.py
+++ format_utilities.py
@@ -1,11 +1,18 @@
 from datetime import datetime
 
 #TODO
-def format_date(date, format_type="YYYY-MM-DD"):
-   """
-    Pendiente de desarrollo
-    """
+def format_date(date):
+   
+    fecha_formato_uno = date.strftime('%Y-%m-%d')
+    print("Formato 1 YYYY-MM-DD:",fecha_formato_uno)
 
+    fecha_formato_dos = date.strftime('%d/%m/%Y')
+    print("Formato 2 DD-MM-YYYY:",fecha_formato_dos)    
+
+    fecha_formato_tres = date.strftime('%m/%d/%Y')
+    print("Formato 2 DD-MM-YYYY:",fecha_formato_tres)    
+   
+    return 
 #TODO
 def format_currency(amount, currency="USD"):
     """
@@ -18,3 +25,5 @@ def format_decimal(number, decimal_places=2):
     return f"{number:.{decimal_places}f}"
 
 
+date = datetime.now()
+format_date(date)
