6dd0b2f2a4a3cfe5ce8576f912ae31f13e98208f
Jaime Urgilez <gustajimmy@hotmail.com>
2025-01-28 10:01:05 -0500

    feat: Agregando funcion calcular días

--- format_utilities.py
+++ format_utilities.py
@@ -18,3 +18,12 @@ def format_decimal(number, decimal_places=2):
     return f"{number:.{decimal_places}f}"
 
 
+
+
+def calculate_days(fech_ini, fech_fin):
+    fecha_inicio = datetime.strptime(fech_ini, "%d/%m/%Y")
+    fecha_final = datetime.strptime(fech_fin, "%d/%m/%Y")
+
+    diferencia_dias = fecha_final - fecha_inicio
+    
+    return abs(diferencia_dias.days)
