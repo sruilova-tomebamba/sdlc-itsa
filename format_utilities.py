19399bcdbe68f554b09ebdaf94790ee0d32b417b
sruilova-tomebamba <stalin.ruilova@tomebamba.com.ec>
2025-01-27 22:07:07 -0500

    feat: Agregando archivo de formateos de fechas, monedas generales apra el proyecto.

---
+++ format_utilities.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+//TODO
+def format_date(date, format_type="YYYY-MM-DD"):
+   """
+    Pendiente de desarrollo
+    """
+
+//TODO
+def format_currency(amount, currency="USD"):
+    """
+    Pendiente de desarrollo
+    """
+
+
+
+def format_decimal(number, decimal_places=2):
+    return f"{number:.{decimal_places}f}"
