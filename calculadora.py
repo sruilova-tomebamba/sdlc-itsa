5628b774f0868eeaecde3227f77d69f84f3f7c26
Daniel Abril <daniel.abril@tomebamba.com.ec>
2025-01-28 08:25:07 -0500

    feat: Función potencia de calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+# Función para calcular la potencia
+def calcular_potencia(base, exponente):
+    """
+    Calcula la potencia de un número.
+    base: el número base.
+    exponente: el número exponente.
+    """
+    return base ** exponente
